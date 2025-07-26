3c129cf612da40ad0d1b11824270175b1b21effb
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2025-07-26 11:40:48 +0000

    arraybasics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// arrray 
+
+const myArr = [0,1,2,3,4,5]
+// console.log(myArr[0]);
+const myheros = ["shaktiman","baalveer"]
+
+const myarr2 = new Array(1,2,3,4)
+
+// console.log(myheros.at(0));
+// console.log(myheros.sort);
+
+// myArr.push(6,7,8,9)
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shift()
+// myArr.shift()
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr);
+
+// const neewARr = myArr.join()
+// console.log(typeof myArr);
+// console.log(typeof neewARr);
+
+// slice , splice 
+
+console.log("A" , myArr);
+
+const myn1 = myArr.slice(1,3);
+
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+console.log("C", myArr);
