11cb4e3484031bf00409f1537c9d8cbbc67f5c2f
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2025-07-28 05:38:59 +0000

    scope in js

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,57 @@
+// var c = 300
+// let a = 200
+// if (true){
+//     let a = 10
+//     const b =20
+//     var c =30
+//     console.log(a,b,c);
+    
+// }
+
+// console.log(a,c);
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+ 
+// function one(){
+//     const username = "vansh"
+//     function two(){
+//         const wesite = "youtube"
+//         console.log(username);
+        
+//     }
+//    //  console.log(website);
+    
+//     two()
+// }
+// // one()
+
+// if(true){
+//     const username = "vansh"
+//     if (username == "vansh"){
+//         const website  = " youtube"
+//         console.log(username , website);
+        
+//     }
+//     console.log(website); // not accesible
+    
+// }
+
+// console.log(username); // not accesible
+
+ // ++++++++++++++++++++++++++  interesting  ++++++++++++++++++++
+
+
+console.log(addone(5));
+ function addone(num){
+    return num+1
+ }
+
+console.log(addTwo(5));
+
+const addTwo = function(num){  // expressions - when func is store in variables
+    return num +2
+}
